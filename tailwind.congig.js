0a70bc99298ba4a6df83fd195a2aba1bf544101c
nafis200 <nafisahamed14@gmail.com>
2024-01-23 21:53:58 -0500

    first commit

---
+++ tailwind.congig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {backgroundImage: {
+        '2.png': "url('/images/2.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }},
+    },
+    plugins: [],
+  }
